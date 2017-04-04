8aacbf4be9b41b0e32ae7ab992e1380edac9a301
F1LT3R <al@pwn.io>
2017-04-03 20:42:42 -0400

    Adding new tests.

---
+++ Markconf.spec.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const chai = require('chai')
+const Markconf = require('app/Markconf.js')
+
+const expect = chai.expect
+
+const specPath = path.relative(process.cwd(), __filename).split('.spec')[0] + '.js'
+
+describe(specPath, () => {
+	it('should provide a Markconf oject', () => {
+		expect(Markconf).to.be.an('object')
+	})
+})
